a8eb2fc967040ae67995f556756d629b43a65b95
microcai <microcaicai@gmail.com>
2014-11-29 22:00:23 +0800

    更新的协议

--- include/avproto/message.hpp
+++ include/avproto/message.hpp
@@ -14,13 +14,25 @@ struct im_decode_error : std::runtime_error
 	int error_code;
 };
 
+struct im_message{
+	bool is_encrypted_message;
+	bool is_group_message;
+	bool is_control_message;
+	bool is_message;
+
+	std::string sender;
+
+	message::message_packet impkt;
+};
+
+
 // 检测是否经过了对称密钥的加密
 bool is_encrypted_message(const std::string& payload);
 
-message::message_packet decode_message(const std::string& payload);
+im_message decode_message(const std::string& payload);
 
 // 解码用的 key 是个 base64 编码的字符串. 加密类型和加密密钥都在里面. 这个 key 字符串由管理员在你进群的时候发送过来
-message::message_packet decode_message(const std::string& encryption_key, const std::string& payload);
+im_message decode_message(const std::string& encryption_key, const std::string& payload);
 
 std::string encode_message(const message::message_packet&);
 std::string encode_message(const std::string& encryption_key, const message::message_packet&);
--- src/message.cpp
+++ src/message.cpp
@@ -2,18 +2,67 @@
 
 #include "avproto/serialization.hpp"
 
-message::message_packet decode_message(const std::string& payload)
-{
-	message::message_packet ret;
-	// payload 的第一个字节表示消息是否加密, 有的话, 返回失败, 必须使用对称加密的密钥解开
-	char type = payload[0];
+enum message_header_type_indicator {
+	TPYE_ENCRYPTED = 0x01,
+	TPYE_GROUP = 0x10,
+	// 发送者的地址也包进去, 供校验用, 可选
+	TPYE_HAS_SENDER = 0x20,
+	// 表明是控制消息, 而非聊天消息
+	// 用于和机器人沟通群管理
+	TYPE_CONTROL_MESSAGE = 0x40,
+};
 
-	switch (type & 0x40)
+bool is_control_message(const std::string& payload)
+{
+	unsigned char type = *((unsigned char*)payload.data());
+
+	return type & TYPE_CONTROL_MESSAGE;
+}
+
+bool is_group_message(const std::string& payload)
+{
+	unsigned char type = *((unsigned char*)payload.data());
+
+	return type & TPYE_GROUP;
+}
+
+bool is_plain_message(const std::string& payload)
+{
+	unsigned char type = *((unsigned char*)payload.data());
+	return type == 0;
+}
+
+bool is_message_packet(const std::string& payload)
+{
+
+}
+
+/*
+ * 第一个字节告诉你包有没有加密, 是不是群消息, 有没有把发送人的 av 地址重复放进去
+ */
+
+im_message decode_message(const std::string& payload)
+{
+	im_message ret;
+	ret.is_encrypted_message = false;
+
+	// payload 的第一个字节表示消息是否加密, 有的话, 返回失败, 必须使用对称加密的密钥解开
+	unsigned char type = *((unsigned char*)payload.data());
+
+	switch (type & TPYE_ENCRYPTED)
 	{
 		throw im_decode_error(0, "encrypted message");
 	}
 
-	if (!ret.ParseFromArray(payload.data()+1, payload.length()-1))
+	if (type & 0xF0)
+	{
+		// 非聊天消息
+	}
+
+	ret.is_control_message = false;
+	ret.is_message = true;
+
+	if (!ret.impkt.ParseFromArray(payload.data()+1, payload.length()-1))
 	{
 		throw im_decode_error(1, "protobuf decode error");
 	}
@@ -23,7 +72,7 @@ message::message_packet decode_message(const std::string& payload)
 std::string encode_message(const message::message_packet& pkt)
 {
 	std::string ret;
-	ret.push_back((char)0x40);
+	ret.push_back((char)0);
 
 
 	if (!pkt.AppendToString(&ret))
