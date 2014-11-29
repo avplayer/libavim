63294f59bac98dbb3892562e0282ab5c4f46ceb5
microcai <microcaicai@gmail.com>
2014-11-29 21:07:14 +0800

    change message name

--- src/message.cpp
+++ src/message.cpp
@@ -2,9 +2,9 @@
 
 #include "avproto/serialization.hpp"
 
-message::avim_message_packet decode_message(const std::string& payload)
+message::message_packet decode_message(const std::string& payload)
 {
-	message::avim_message_packet ret;
+	message::message_packet ret;
 	// payload 的第一个字节表示消息是否加密, 有的话, 返回失败, 必须使用对称加密的密钥解开
 	char type = payload[0];
 
@@ -20,7 +20,7 @@ message::avim_message_packet decode_message(const std::string& payload)
 	return ret;
 }
 
-std::string encode_message(const message::avim_message_packet& pkt)
+std::string encode_message(const message::message_packet& pkt)
 {
 	std::string ret;
 	ret.push_back((char)0x40);
