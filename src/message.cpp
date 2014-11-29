0f556d5651be9e83f75166fcac81d67e4a17b0ca
microcai <microcaicai@gmail.com>
2014-11-29 19:52:09 +0800

    把 im.proto 的名称空间放 message::

--- src/message.cpp
+++ src/message.cpp
@@ -2,9 +2,9 @@
 
 #include "serialization.hpp"
 
-proto::avim_message_packet decode_message(const std::string& payload)
+message::avim_message_packet decode_message(const std::string& payload)
 {
-	proto::avim_message_packet ret;
+	message::avim_message_packet ret;
 	// payload 的第一个字节表示消息是否加密, 有的话, 返回失败, 必须使用对称加密的密钥解开
 	char type = payload[0];
 
@@ -20,7 +20,7 @@ proto::avim_message_packet decode_message(const std::string& payload)
 	return ret;
 }
 
-std::string encode_message(const proto::avim_message_packet& pkt)
+std::string encode_message(const message::avim_message_packet& pkt)
 {
 	std::string ret;
 	ret.push_back((char)0x40);
